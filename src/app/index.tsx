c8c1bf0f83de3aca6e7047e72a9a80b56e7f8578
Fabricio <fabricioliquesley15@gmail.com>
2024-04-05 14:01:59 -0300

    feat: create access page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,35 @@
-import { View, Text } from "react-native";
+import { View, Image, StatusBar } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Link } from "expo-router";
+
+import { Input } from "@/components/input";
+import { Button } from "@/components/button";
+import { colors } from "@/styles/colors";
+
+const logoImage = require("@/assets/logo.png");
 
 export default function Home() {
   return (
-    <View className="flex-1 justify-center items-center bg-green-500">
-      <Text className="text-white text-2xl">Hello word!</Text>
+    <View className="flex-1 justify-center items-center bg-green-500 p-8">
+      <StatusBar barStyle={"light-content"} />
+      <Image source={logoImage} className="h-16" resizeMode="contain" />
+      <View className="w-full mt-12 gap-3">
+        <Input>
+          <MaterialCommunityIcons
+            name="ticket-confirmation-outline"
+            color={colors.green[200]}
+            size={20}
+          />
+          <Input.Field placeholder="Código do ingresso" />
+        </Input>
+        <Button title="Acessar credencial" />
+        <Link
+          href="/register"
+          className="text-gray-100 text-base text-center mt-8"
+        >
+          Ainda não possui ingresso?
+        </Link>
+      </View>
     </View>
   );
 }
